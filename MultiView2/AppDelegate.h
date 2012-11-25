73f8f8536510a2fa7704338c42ae6bba2d0d4d6b
liuwei <liuw79@gmail.com>
2012-11-25 14:07:09 +0800

    Initial Commit

---
+++ MultiView2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultiView2
+//
+//  Created by liuwei on 12-11-25.
+//  Copyright (c) 2012年 liuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
